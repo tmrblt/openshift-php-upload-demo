55a2a8684b4f1e144d8037fb5c2879d9c8d695a1
Christian Hernandez <christianh814@users.noreply.github.com>
2017-01-23 12:31:44 -0800

    Create listfiles.php

---
+++ listfiles.php
@@ -0,0 +1,16 @@
+<?php
+$datadir = /opt/app-root/src/uploaded
+/* Display all file except core application files */
+if ($handle = opendir($datadir)) {
+   while (false !== ($file = readdir($handle))) {
+          /* This is to exclude files from being shown on the screen - probably a better way of doing this */
+          if ($file != "." && $file != ".." && $file != "index.php" && $file != "OpenStack.php" && $file != "README.md" && $file != "upload.php" && $file != ".openshift" && $file != ".vimrc" && $file != ".bash_profile" && $file != ".bash_history" && $file != "config.php" && $file != "ufo.php") {
+            $thelist .= '<li><a href="'.$file.'">'.$file.'</a></li>';
+            /* $thelist .= '<li><a href="'. $datadir . $file . '">'.$file.'</a></li>'; */
+          }
+       }
+  closedir($handle);
+  }
+echo "<h4>List of files:</h4>";
+echo "<ul>" . $thelist . "</ul>";
+?>
