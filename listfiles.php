6a276a4efb06053867906e5f751c48404037611f
Christian Hernandez <christianh814@users.noreply.github.com>
2017-01-23 12:53:41 -0800

    Update listfiles.php

--- listfiles.php
+++ listfiles.php
@@ -5,7 +5,7 @@ if ($handle = opendir($datadir)) {
    while (false !== ($file = readdir($handle))) {
           /* This is to exclude files from being shown on the screen - probably a better way of doing this */
           if ($file != "lost+found" && $file != "." && $file != ".." && $file != "index.php" && $file != "OpenStack.php" && $file != "README.md" && $file != "upload.php" && $file != ".openshift" && $file != ".vimrc" && $file != ".bash_profile" && $file != ".bash_history" && $file != "config.php" && $file != "ufo.php") {
-            $thelist .= '<li><a href=/uploaded/"'.$file.'">'.$file.'</a></li>';
+            $thelist .= '<li><a href="/uploaded/'.$file.'">'.$file.'</a></li>';
             /* $thelist .= '<li><a href="'. $datadir . $file . '">'.$file.'</a></li>'; */
           }
        }
