8a3e978bc9fa1d7e6c0a7699ce771294c4f0bfa2
Christian Hernandez <christianh814@users.noreply.github.com>
2017-03-16 12:19:43 -0700

    added support for glusterfs volumes

--- listfiles.php
+++ listfiles.php
@@ -4,7 +4,7 @@ $datadir = "/opt/app-root/src/uploaded";
 if ($handle = opendir($datadir)) {
    while (false !== ($file = readdir($handle))) {
           /* This is to exclude files from being shown on the screen - probably a better way of doing this */
-          if ($file != ".gitkeep" && $file != "lost+found" && $file != "." && $file != ".." && $file != "index.php" && $file != "OpenStack.php" && $file != "README.md" && $file != "upload.php" && $file != ".openshift" && $file != ".vimrc" && $file != ".bash_profile" && $file != ".bash_history" && $file != "config.php" && $file != "ufo.php") {
+          if ($file != ".gitkeep" && $file != ".trashcan" && $file != "lost+found" && $file != "." && $file != ".." && $file != "index.php" && $file != "OpenStack.php" && $file != "README.md" && $file != "upload.php" && $file != ".openshift" && $file != ".vimrc" && $file != ".bash_profile" && $file != ".bash_history" && $file != "config.php" && $file != "ufo.php") {
             $thelist .= '<li><a href="/uploaded/'.$file.'">'.$file.'</a></li>';
             /* $thelist .= '<li><a href="'. $datadir . $file . '">'.$file.'</a></li>'; */
           }
